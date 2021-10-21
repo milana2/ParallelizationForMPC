6472b123b51fe1ee7320327c7908e0d7b3cb4fc6
Muhammad Ishaq <ishaq@ishaq.pk>
2021-10-21 17:55:07 +0100

    create biometric_fast.py, see full commit message
    I tried running it but I got an error saying TypeError: 'type' object is not subscriptable
    
    Since this is the same error I get when I try to run biometric.py, I assume this is a problem with my system only.

---
+++ benchmarks/biometric_fast.py
@@ -0,0 +1,68 @@
+import typing
+
+def biometric_matching_fast(D, N, C:list[int], C_sqr_sum:int, two_C:list[int], S: List[int], S_sqr_sum: list[int]):
+  """
+  Computes biometric matching
+
+  This version uses some preprocessed data from parties to provide faster biometric matching
+
+  :param int D: the cardinality of a feature vector, Usually small, e.g. 4
+  :param int N: number of features in the database e.g. usually 1024, 2048, 4096, etc
+  :param list[int] C: query feature vector, we need to find closest match to this vector in the DB, comes from client (Alice)
+  :param int C_sqr_sum: sum of squares of elements of `C` e.g. if `C={1, 2, 3, 4}`, then `C_sqr_sum is: 1*1 + 2*2 + 3*3 + 4*4 = 30`
+   client passes it pre-processed to to save gates in circuit
+  :param list[int] two_C: same as `C` except that each element is multipled by 2, e.g. if `C={1, 2, 3, 4}`, then 
+   `two_C = {2, 4, 6, 8}`. client passes it preprocessed to save gates
+  :param list[int] S: the database of features, it has N * D elements i.e. N features and each feature vector has D elements,
+   this comes from server (Bob)
+  :param list[int] S_sqr_sum: has N elements, each element is sum of squares of corresponding feature elements e.g. say
+   S={{1, 2, 3, 4}, {5, 6, 7, 8}}, then S_sqr_sum={1*1 + 2*2 + 3*3 + 4*4, 5*5 + 6*6 + 7*7 + 8*8} = {30, 174}
+  
+  """ 
+
+  differences: list[int] = [0] * D
+  for i in range(N):
+    a_sqr_plus_b_sqr: int = S_sqr_sum[i] + C_sqr_sum
+    two_a_b: int = 0
+    
+    for j in range(D):
+      tmp: int = S[i*D+j] * two_C[j]
+      two_a_b = two_a_b + tmp
+
+    this_diff: int = a_sqr_plus_b_sqr - two_a_b
+    differences[i] = this_diff
+
+
+    min_diff: int = differences[0]
+    min_index: int = 0
+
+    for k in range(N):
+      if(differences[k] < min_diff):
+        min_diff = differences[k]
+        min_index = k
+
+  return (min_diff, min_index)
+
+
+
+def test_biometric_matching_fast(D, N, C, S):
+  """
+  just a convenience method for testing, computes the pre-processing data for the actual call
+  """
+  two_C = [0] * D
+  C_sqr_sum = 0
+  S_sqr_sum = [0] * N
+  for i in range(D):
+    two_C[i] = 2 * C[i]
+    C_sqr_sum = C_sqr_sum + (C[i] * C[i])
+
+  for i in range(N):
+    for j in range(D):
+      S_sqr_sum[i] = S_sqr_sum[i] + (S[i*D+j] * S[i*D+j])
+
+  print(biometric_matching__fast(D, N, C, C_sqr_sum, two_C, S, S_sqr_sum))
+
+
+C = [1,2,3,4]
+S = [4,5,2,10,2,120,4,10,99,88,77,66,55,44,33,22]
+test_biometric_matching_fast(4, 4, C, S)
