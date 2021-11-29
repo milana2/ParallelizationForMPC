cdd6c9ac700a82315ef9c04ff4823da78688a59d
Ana Milanova <milana2@rpi.edu>
2021-11-29 09:43:12 -0500

    MPC-correct version of PSI.

--- benchmarks/psi.py
+++ benchmarks/psi.py
@@ -5,16 +5,22 @@ import typing
 # requires: no repetition of elements in either A or B
 # requires: len(A) = SA, len(B) = SB
 def psi(A: list[int], SA, B: list[int], SB) -> list[int]:
+  dummy : int = 0
   result: list[int] = []
   for i in range(0,SA):
+    flag : Bool = False
     for j in range(0,SB):
       if A[i] == B[j]:
-        #overloaded +. This is append actually.
-        result = result + [A[i]]
+        flag = True
+    val : int = dummy
+    if flag:
+      val : int = A[i]
+    #overloaded +. This is append actually.
+    result = result + [val]
   return result
 
-A = [1,2,3]
-B = [2]
-intersect = psi(A,3,B,1)
+A = [4,2,3,1,10]
+B = [2,10,3,4,5,6,7]
+intersect = psi(A,5,B,7)
 print(intersect)
 
