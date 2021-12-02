427fd887e9c1de236699847c184346f6c828a69c
Benjamin Levy <blevy@protonmail.com>
2021-12-02 00:48:58 -0500

    Update PSI tests

--- compiler/tests/stages/psi/restricted_ast.py
+++ compiler/tests/stages/psi/restricted_ast.py
@@ -1,7 +1,13 @@
 def foo(A, SA, B, SB):
+    dummy = 0
     result = []
     for i in range(0, SA):
+        flag = False
         for j in range(0, SB):
             if (A[i] == B[j]):
-                result = (result + [A[i]])
+                flag = True
+        val = dummy
+        if flag:
+            val = A[i]
+        result = (result + [val])
     return result
