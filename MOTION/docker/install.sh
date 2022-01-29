df811d6c6b3aa001f0485544732dc1f05ded226d
Benjamin Sherman <benjamin@bensherman.io>
2022-01-28 19:57:05 -0500

    Remove raw clone of MOTION

--- MOTION/docker/install.sh
+++
@@ -1,6 +0,0 @@
-# get MOTION
-git clone --recursive --config core.autocrlf=input https://github.com/encryptogroup/MOTION.git
-cd MOTION
-# build MOTION
-cmake -DMOTION_BUILD_TESTS=On -DMOTION_BUILD_EXE=On -DMOTION_LINK_TCMALLOC=On -B build
-cmake --build build -- -j 4
