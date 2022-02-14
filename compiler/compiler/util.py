2af7159861dcebb9cb604701d67befd9f434c165
Benjamin Sherman <benjamin@bensherman.io>
2022-02-13 20:47:33 -0500

    Revert "Make assert_never more robust"
    This reverts commit 34ed6d150c3cd3e74d2bef5318d0db6a7da5ac6c.

--- compiler/compiler/util.py
+++ compiler/compiler/util.py
@@ -1,6 +1,6 @@
-from typing import Any, NoReturn
+from typing import NoReturn
 
 
 # https://github.com/python/mypy/issues/5818#issue-372451340
-def assert_never(x: Any) -> NoReturn:
+def assert_never(x: NoReturn) -> NoReturn:
     assert False, "Unhandled type: {}".format(type(x).__name__)
