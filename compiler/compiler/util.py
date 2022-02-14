34ed6d150c3cd3e74d2bef5318d0db6a7da5ac6c
Benjamin Sherman <benjamin@bensherman.io>
2022-02-13 19:29:55 -0500

    Make assert_never more robust

--- compiler/compiler/util.py
+++ compiler/compiler/util.py
@@ -1,6 +1,6 @@
-from typing import NoReturn
+from typing import Any, NoReturn
 
 
 # https://github.com/python/mypy/issues/5818#issue-372451340
-def assert_never(x: NoReturn) -> NoReturn:
+def assert_never(x: Any) -> NoReturn:
     assert False, "Unhandled type: {}".format(type(x).__name__)
