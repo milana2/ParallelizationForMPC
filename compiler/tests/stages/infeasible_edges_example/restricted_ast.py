eaabc15be2b2d993571e2f1e7dded77fbbb77955
Benjamin Sherman <benjamin@bensherman.io>
2022-03-30 13:24:39 -0400

    Clean up tests/context.py

--- compiler/tests/stages/infeasible_edges_example/restricted_ast.py
+++
@@ -1,7 +0,0 @@
-def foo(A: shared[list[int]], B: shared[list[int]], C: shared[list[int]], D: shared[list[int]], N: plaintext[int]) -> tuple[shared[list[int]], shared[list[int]], shared[list[int]], shared[list[int]]]:
-    for i: plaintext[int] in range(0, N):
-        A[i] = (B[i] + 10)
-        B[i] = (A[i] * D[(i - 1)])
-        C[i] = (A[i] * D[(i - 1)])
-        D[i] = (B[i] * C[i])
-    return (A, B, C, D)
