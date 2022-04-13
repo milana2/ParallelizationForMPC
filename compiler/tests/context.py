e5d8cb9f968256d7100ba060e4dd3683672e3c73
Benjamin Sherman <benjamin@bensherman.io>
2022-04-12 22:40:31 -0400

    Enable chapterfour_figure_12 benchmark

--- compiler/tests/context.py
+++ compiler/tests/context.py
@@ -12,8 +12,6 @@ SKIPPED_TESTS = [
     "biometric_vectorized",
     # These benchmarks fail because they involve appending to arrays
     "convex_hull",
-    # TODO: figure out why these tests are failing:
-    "chapterfour_figure_12",
     # The following tests cause issues since they don't loop from 0
     "longest_1s",
     "longest_even_0",
